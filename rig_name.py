00a05322c42194d5eb698bd4d83eeea5a36272f1
Hairi-Aramata <hairi_alan_aramata@yahoo.co.jp>
2023-03-10 14:01:04 -0800

    rigtype2

--- rig_name.py
+++ rig_name.py
@@ -123,7 +123,7 @@ class RigType(NameBase):
 
     def validate(self):
         if self.name not in VALID_RIG_TYPES:
-            logger.error('Control types must match: {}'.format(VALID_RIG_TYPES))
+            logger.error('RIG types must match: {}'.format(VALID_RIG_TYPES))
             return
 
     def output(self):
