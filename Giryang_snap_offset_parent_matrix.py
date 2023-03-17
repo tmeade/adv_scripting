0061ad41adaef5e1f8d40e99d95fa53819b48eda
GiryangB <v1312v@gmail.com>
2023-03-16 22:25:59 -0700

    giryang_edit

--- Giryang_snap_offset_parent_matrix.py
+++ Giryang_snap_offset_parent_matrix.py
@@ -2,17 +2,24 @@ import maya.cmds as cmds
 import maya.api.OpenMaya as om
 
 def snap_offset_parent_matrix():
+    """
+     Description:
+         Set target's matrix to source's matrix (snapping without changing values)
+     Args:
+         sourceObj : joint1
+         targetObj : nurbsCircle1
+    """
     
     # joint1 can be sourceObj
     joint_world_mtx = cmds.xform("joint1", query=True, worldSpace=True, matrix=True)
     joint_math_mtx = om.MMatrix(joint_world_mtx)
     # nurbsCircle1 can be targetObj or offset group for snapping
-    target_world_mtx = cmds.xform("group3", query=True, worldSpace=True, matrix=True)
+    target_world_mtx = cmds.xform("nurbsCircle1", query=True, worldSpace=True, matrix=True)
     target_math_mtx = om.MMatrix(target_world_mtx)    
     
     #offset_matrix = target_math_mtx * joint_math_mtx.inverse()
     
     # Set target's matrix to source's matrix
-    cmds.setAttr('group3.offsetParentMatrix',joint_math_mtx, type='matrix')
+    cmds.setAttr('nurbsCircle1.offsetParentMatrix',joint_math_mtx, type='matrix')
     
 snap_offset_parent_matrix()
