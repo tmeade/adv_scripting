9d9285649b95bde1836ebd38dc7329b33f691a4c
chachajodi <chacha.jodi@gmail.com>
2023-05-03 16:34:37 -0700

    leg

--- rig/appendages/leg.py
+++ rig/appendages/leg.py
@@ -36,9 +36,11 @@ class Leg(two_bone_fkik.TwoBoneFKIK):
         # ball_joint
         # toe_end_joint
         # TODO: Validate/test that there is a parent joint here.
+        self.upleg_joint = self.bnd_joints['start_joint']
 		self.ankle_joint = self.bnd_joints['end_joint']
 		self.ball_joint = cmds.listRelatives(self.ankle_joint)
 		self.toeEnd_joint = cmds.listRelatives(self.ball_joint)
+		mc.addAttr(self.output, longName='foot_matrix', attributeType='matrix')
 
 
 	    # Extract a control skeleton for the fk
@@ -48,6 +50,20 @@ class Leg(two_bone_fkik.TwoBoneFKIK):
 		                                                    self.num_upperTwist_joints,
 															self.num_lowerTwist_joints)
 
+	def build(self):
+        # TODO: name should be more accessable.  The renaming utility is basing the rename on
+        # the joint name and this does not work well in this case.  rigname should be:
+        #            rn.RigName(
+        #                    element='root',
+        #                    rig_type='ctrl',
+        #                    maya_type='transform'))
+        self.foot_ctrl = utils.create_fk_control(self.start_joint,
+                                                 connect_output=f'{self.output}.foot_matrix')
+
+    def connect_outputs(self):
+        # Connect the start matrix on the output node to the skeleton
+        cmds.connectAttr(f'{self.output}.foot_matrix', f'{self.start_joint}.offsetParentMatrix')
+    
 
 		# Extract a control skeleton for the ik
 		# self.ik_skeleton = utils.create_control_joints_from_skeleton(self.bnd_joints['start_joint'],
