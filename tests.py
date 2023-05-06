816600aa916c05a218d7eb4c872276a82e45ee29
tmeade <tmeade@mac.com>
2023-05-05 18:03:01 -0700

    added root appendage tests

--- tests.py
+++ tests.py
@@ -1,10 +1,13 @@
+import logging
+logger = logging.getLogger()
 import unittest
 import maya.standalone
 maya.standalone.initialize()
 import maya.cmds as cmds
-
 import rig_name
 import utilities
+import adv_scripting.rig.appendages.root as root
+
 
 
 class TestRigName(unittest.TestCase):
@@ -25,7 +28,7 @@ class TestRigName(unittest.TestCase):
 
     def test_fullname(self):
         fullname = rig_name.RigName(full_name = 'lt_front_arm_ik_ctrl_curve_20')
-        self.assertEqual(fullname.output, 'lt_front_arm_ik_ctrl_curve_20')
+        self.assertEqual(fullname.output(), 'lt_front_arm_ik_ctrl_curve_20')
 
 
 class TestUtilities(unittest.TestCase):
@@ -41,27 +44,25 @@ class TestUtilities(unittest.TestCase):
     def test_create_fk_control_position(self):
         self.assertEqual(self.position, [5, 5, 10])
 
+class TestRootAppendage(unittest.TestCase):
+    def setUp(self):
+        self.joint = cmds.joint(p=(50, 50, 10))
+        self.root = root.Root('test_root', self.joint)
 
+    def test_root_placement_position(self):
+        position = cmds.xform(self.root.controls['fk']['root_ctrl'], query=True, ws=True, t=True)
+        self.assertEqual(position, [50, 50, 10])
+
+    def test_root_translation(self):
+        cmds.xform(self.root.controls['fk']['root_ctrl'], ws=True, t=(20, 20, 20))
+        position = cmds.xform(self.joint, query=True, ws=True, t=True)
+        logger.info(position)
+        self.assertEqual(position, [20.0, 20.0, 20.0])
+
+    def test_root_rotation(self):
+        cmds.xform(self.root.controls['fk']['root_ctrl'], ws=True, ro=(10, 10, 10))
+        rotation = cmds.xform(self.joint, query=True, ws=True, ro=True)
+        self.assertEqual(rotation, [10.0, 10.0, 10.0])
 
 if __name__ == '__main__':
     unittest.main()
-
-'''
-    test1 = RigName(side=side_type, region='front', element='arm', control_type='ik', rig_type=RigType('ctrl'), maya_type='curve', position=20)
-    logger.debug('--- test1 ---')
-    logger.debug(f'Full name: {test1.full_name}')
-    logger.debug(f'Name: {test1.name}')
-    logger.debug(f'{test1}')
-
-    test2 = RigName(full_name = 'lt_front_arm_ik_ctrl_curve_20')
-    logger.debug('--- test2 ---')
-    logger.debug(f'Full name: {test2.full_name}')
-    logger.debug(f'Name: {test2.name}')
-    logger.debug(f'{test2}')
-
-    test3 = RigName(full_name = 'LeftHandIndex1')
-    logger.debug('--- test3 ---')
-    logger.debug(f'Full name: {test3.full_name}')
-    logger.debug(f'Name: {test3.name}')
-    logger.debug(f'{test3}')
-'''
