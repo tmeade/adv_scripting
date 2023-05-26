5d9973b8af83ee63610d2559455fc722d26419ca
GiryangB <v1312v@gmail.com>
2023-05-25 20:47:34 -0700

    spine_addsomeSelf

--- rig/appendages/spine.py
+++ rig/appendages/spine.py
@@ -19,54 +19,54 @@ class Spine(rap.Appendage):
         rap.Appendage.__init__(self, appendage_name, start_joint, input_matrix)
 
     def setup(self):
-
+        
         # Get the selected joint
         selected_joint = self.start_joint
         child_joint = cmds.listRelatives(selected_joint, ad=True, type="joint")
 
         dv_prefix = 'driver_'
         fk_prefix = 'fk_follow_'
-
+        
         """dv_prefix = rn.RigName(element=self.element, side=self.side,
             control_type='driver', rig_type='jnt', maya_type=None) TypeError: expected string or bytes-like object  """
+        
 
-
-        # Copy and rename the joint hierarchy
+        # Copy and rename the joint hierarchy 
         dv_root_joint = utils.copy_rename_joint_hierarchy(selected_joint, dv_prefix)
-
-
+        
+        
         # make list for ik, fk joints children
         dvchild_list = cmds.listRelatives(dv_root_joint, ad=True, type="joint")
 
         children = cmds.listRelatives(selected_joint, c=True, type="joint")
-
+        
         utils.delete_useless_joint(dv_root_joint, 'spine')
         """
         (root, keyword)
         root : root joint for delete useless joints children
         keyword : Select keywords not to delete
         """
-
+        
         ik_spine_joints = utils.copy_rename_joint_hierarchy(dv_root_joint, fk_prefix)
-
+        
         cmds.parent(ik_spine_joints, world=True)
         ik_spine_joints_list = cmds.ls(type='joint')
         ik_spine_joints_list = [joint for joint in ik_spine_joints_list if fk_prefix in joint]
-
+        
         # list for Unparent
         spine_joints_list = cmds.listRelatives(dv_root_joint, ad=True, type='joint')
         spine_joints_list.append(dv_root_joint)
-
+        
 
         # Unparent each joint in the hierarchy
         cmds.parent(spine_joints_list, world=True)
-
-        # Delete [odd] joint for binding driver joint
+        
+        # Delete [odd] joint for binding driver joint 
         for i, joint in enumerate(spine_joints_list):
             if i % 2 != 0:
                 cmds.delete(spine_joints_list[i])
-
-
+                        
+        
         # Get the selected joint group
         sel_joints = cmds.ls(ik_spine_joints, type="joint")
         # Get the hierarchy of the selected joints
@@ -101,32 +101,32 @@ class Spine(rap.Appendage):
         # save on valuable Joint Hierarchy
         root_joint = cmds.ls(ik_spine_joints, dag=True)
         childs_joint = cmds.listRelatives(root_joint, ad=True, type="joint")
-
+        
         # Clean curves, history
         cmds.delete(curve_loft, constructionHistory=True)
 
         cmds.delete(orig_curve)
         cmds.delete(copy_curve)
-
-        cmds.parent(root_joint[0], self.appendage_grp)
+        
+        cmds.parent(root_joint[0], "spine_grp")
         for joint in childs_joint:
-            cmds.parent(joint, self.appendage_grp)
-
+            cmds.parent(joint, "spine_grp")
+        
         """
         cmds.select(curve_loft[0])
         cmds.select(root_joint, add=True)
         cmds.UVPin()
         """
-
+        
         # Tried to make as node without select but not moving
         tempBS = cmds.blendShape(curve_loft[0], n='tempBS')
         sPin = cmds.createNode('uvPin', n='spineUvPin')
         cmds.setAttr("spineUvPin.tangentAxis", 1)
         cmds.connectAttr('spineSetupSurfaceShape.worldSpace', f'{sPin}.deformedGeometry')
         cmds.connectAttr('spineSetupSurfaceShapeOrig.local', f'{sPin}.originalGeometry')
-
-        cmds.delete(tempBS)
-
+        
+        cmds.delete(tempBS) 
+        
         for i, joint in enumerate(root_joint):
             cmds.setAttr(f'{joint}.inheritsTransform',0)
             cmds.connectAttr(f'spineUvPin.outputMatrix[{i}]', f'{joint}.offsetParentMatrix')
@@ -141,72 +141,77 @@ class Spine(rap.Appendage):
         cmds.setAttr(f'{sPin}.coordinate[2].coordinateV', 0.470)
         cmds.setAttr(f'{sPin}.coordinate[3].coordinateV', 0.751)
         cmds.setAttr(f'{sPin}.coordinate[4].coordinateV', 1)
-
+        
         dv_spine_joints_list = cmds.ls(type='joint')
         dv_spine_joints_list = [joint for joint in dv_spine_joints_list if not fk_prefix in joint]
         dv_spine_joints_list = [joint for joint in dv_spine_joints_list if dv_prefix in joint]
-
-
+        
+        
         cmds.skinCluster(dv_spine_joints_list[0],dv_spine_joints_list[1],dv_spine_joints_list[2],curve_loft[0], n="ik_skinC")
         cmds.skinCluster("ik_skinC", e=True, mi=3)
 
         cmds.setAttr("spineSetupSurface.visibility", False)
-        cmds.parent(curve_loft[0], self.appendage_grp)
+        cmds.parent(curve_loft[0], "spine_grp")
         for joint in dv_spine_joints_list:
-            cmds.parent(joint, self.appendage_grp)
-
-
+            cmds.parent(joint, "spine_grp")
+        
+               
         # No additional setup needed for setup()
         return
-
+        
     def build(self):
-
+        
         dv_prefix = 'driver_'
         fk_prefix = 'fk_follow_'
-
+        
         # Implement build method here
         spine_joints_list = cmds.ls(type='joint')
         spine_joints_list = [joint for joint in spine_joints_list if 'spine_' in joint]
         spine_joints_list = [joint for joint in spine_joints_list if not fk_prefix or not dv_prefix in joint]
-
+        
         ik_spine_joints_list = cmds.ls(type='joint')
         ik_spine_joints_list = [joint for joint in ik_spine_joints_list if fk_prefix in joint]
-
+        
         dv_spine_joints_list = cmds.ls(type='joint')
         dv_spine_joints_list = [joint for joint in dv_spine_joints_list if not fk_prefix in joint]
         dv_spine_joints_list = [joint for joint in dv_spine_joints_list if dv_prefix in joint]
-
-        spine_ik_ls = []
-        spine_fk_ls = []
-
-        for i, joint in enumerate(dv_spine_joints_list):
-            self.ik_spine_transform = utils.create_fk_control(dv_spine_joints_list[i],parent_control=self.controls_grp)
-            spine_ik_ls.append(self.ik_spine_transform)
-
-        for i, joint in enumerate(spine_joints_list):
-            self.fk_spine_transform = utils.create_fk_control(spine_joints_list[i],parent_control=self.controls_grp)
-            spine_fk_ls.append(self.fk_spine_transform)
-
-        for btrans, ijoint in zip(spine_fk_ls, ik_spine_joints_list):
+        
+        self.spine_ik_ls = []
+        self.spine_fk_ls = []
+                   
+        for i, joint in enumerate(dv_spine_joints_list):    
+            self.ik_spine_transform = utils.create_fk_control(dv_spine_joints_list[i],parent_control="controls_grp")
+            self.spine_ik_ls.append(self.ik_spine_transform)
+            
+        for i, joint in enumerate(spine_joints_list):    
+            self.fk_spine_transform = utils.create_fk_control(spine_joints_list[i],parent_control="controls_grp")
+            self.spine_fk_ls.append(self.fk_spine_transform)
+                   
+        for btrans, ijoint in zip(self.spine_fk_ls, ik_spine_joints_list):
             mt.matrix_parent_constraint(ijoint, btrans)
-
-        for i in range(len(spine_ik_ls)):
-            if i+1 < len(spine_ik_ls):
-                cmds.parent(spine_ik_ls[i+1], spine_ik_ls[i])
-
-
+        
+        for i in range(len(self.spine_ik_ls)):
+            if i+1 < len(self.spine_ik_ls):
+                cmds.parent(self.spine_ik_ls[i+1], self.spine_ik_ls[i])
+        print(self.spine_fk_ls[-1])
+            
+                            
 
     def cleanup(self):
         # Implement cleanup method here
         pass
 
     def connect_inputs(self):
-        # Implement connect_outputs method here
-        pass
+        # Implement connect_inputs method here
+
+        if self.input_matrix:
+            matrix_tools.matrix_parent_constraint(f'{self.input}.input_matrix', self.spine_fk_ls[-1])
+        
 
     def connect_outputs(self):
         # Implement connect_outputs method here
+        # Connect the start matrix on the output node to the skeleton
         pass
 
 # Maya Test
-# spine = Spine("spine", "spine_bnd_jnt_01")
+#spine = Spine("spine", "spine_bnd_jnt_01")
